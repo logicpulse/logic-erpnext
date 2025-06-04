3d96e74618a6f22000ac866c332260bdce2c7d8e
Hailes Mauricio <hailes.mauricio@hotmail.com>
2025-06-04 13:01:54 +0000

    ✨ add new catalog generation features and assets, including PDF and image files

--- erpnext/test/page/gerador_de_catalogos/gerador_de_catalogos.js
+++ erpnext/test/page/gerador_de_catalogos/gerador_de_catalogos.js
@@ -7,12 +7,12 @@ frappe.pages['gerador-de-catalogos'].on_page_load = function (wrapper) {
 	//let $btn = page.set_primary_action('New', () => show(), 'octicon octicon-plus')
 	page.add_action_item('access', async () => await generate('access', 'Gestão de Acessos', 'E:P'));
 	page.add_action_item('time', async () => await generate('time', 'Gestão de Assiduidade', 'E:T'));
-	page.add_action_item('q.track', async () => await generate('Gestão de Filas de Espera', 'E:T'));
-	page.add_action_item('fleet', async () => await generate('Gestão de Frotas', 'E:T'));
-	page.add_action_item('logicpos', async () => await generate('POS', 'E:T'));
-	page.add_action_item('library', async () => await generate('Gestão de Bibliotecas', 'E:T'));
-	page.add_action_item('factory', async () => await generate('Gestão industrial', 'E:T'));
-	page.add_action_item('cover', async () => await generate('Gestão de Acessos', 'E:P'));
+	page.add_action_item('q.track', async () => await generate('q', 'Gestão de Filas de Espera', 'E:T'));
+	page.add_action_item('fleet', async () => await generate('fleet', 'Gestão de Frotas', 'E:T'));
+	page.add_action_item('logicpos', async () => await generate('pos', 'POS', 'E:T'));
+	page.add_action_item('library', async () => await generate('library', 'Gestão de Bibliotecas', 'E:T'));
+	page.add_action_item('factory', async () => await generate('factory', 'Gestão industrial', 'E:T'));
+	page.add_action_item('cover', async () => await getCover());
 	page.add_action_item('back cover', async () => await generate('Gestão de Acessos', 'E:P'));
 	page.add_action_item('index', async () => await generate('Gestão de Acessos', 'E:P'));
 	//page.add_menu_item('Send Email', () => open_email_dialog())
@@ -22,15 +22,14 @@ frappe.pages['gerador-de-catalogos'].on_page_load = function (wrapper) {
 
 async function generate(ref, sheet_name, cell_range) {
 	const spreadsheet_id = '1Nm6YatjJrugBxM38yaXlIJgLHfVAMCnnMLw83lga5YQ'
-	console.log('catalog_access');
 	const pais = document.getElementById('pais') ? document.getElementById('pais').value : null;
 	const tipoPreco = document.getElementById('tipo_preco') ? document.getElementById('tipo_preco').value : null;
 
-	console.log('Pais selecionado:', pais);
-	console.log('Tipo de preço selecionado:', tipoPreco);
+	// console.log('Pais selecionado:', pais);
+	// console.log('Tipo de preço selecionado:', tipoPreco);
 
 	frappe.call({
-		method: 'erpnext.test.page.gerador_de_catalogos.gerador_de_catalogos.catalog_access',
+		method: 'erpnext.test.page.gerador_de_catalogos.gerador_de_catalogos.get_catalog',
 		args: {
 			ref: ref,
 			spreadsheet_id: spreadsheet_id,
@@ -41,15 +40,30 @@ async function generate(ref, sheet_name, cell_range) {
 		},
 		callback: function (res) {
 			if (res.message) {
-				console.log('res:', res);
-				console.log('message:', res.message);
+				// console.log('res:', res);
+				// console.log('message:', res.message);
+				console.log('Ficheiro gerado com sucesso!');
+			} 
+		}
+	});
+}
 
-				// window.open(res.message, '_blank');
+async function getCover() {
+	const pais = document.getElementById('pais') ? document.getElementById('pais').value : null;
+	const tipoPreco = document.getElementById('tipo_preco') ? document.getElementById('tipo_preco').value : null;
+
+	frappe.call({
+		method: 'erpnext.test.page.gerador_de_catalogos.gerador_de_catalogos.get_cover',
+		args: {
+			country: pais,
+			price_type: tipoPreco
+		},
+		callback: function (res) {
+			if (res.message) {
+				console.log('Capa gerada com sucesso!');
+			} else {
+				console.error('Erro ao gerar a capa:', res);
 			}
-			// if (resp.message) {
-			// 	console.log(resp.message);
-			// 	// faça algo com os dados
-			// }
 		}
 	});
 }
