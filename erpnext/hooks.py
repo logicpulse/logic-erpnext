077c41e9d2c8664ae9ce40e684fda51cb19d95ef
Hailes Mauricio <hailes.mauricio@hotmail.com>
2025-06-05 10:57:07 +0000

    ♻️ code structure for improved readability and maintainability

--- erpnext/hooks.py
+++ erpnext/hooks.py
@@ -24,7 +24,10 @@ add_to_apps_screen = [
 develop_version = "15.x.x-develop"
 
 app_include_js = "erpnext.bundle.js"
-app_include_css = "erpnext.bundle.css"
+app_include_css = [
+    "erpnext.bundle.css",
+    "/assets/erpnext/css/custom.css"
+]
 web_include_js = "erpnext-web.bundle.js"
 web_include_css = "erpnext-web.bundle.css"
 email_css = "email_erpnext.bundle.css"
